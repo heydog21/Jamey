2f4e6902ac11e53002f236ea428ae155e9aa60b5
heydog21 <61715655+heydog21@users.noreply.github.com>
2020-03-02 17:59:38 -0500

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_HeywardJamey.py
@@ -0,0 +1,14 @@
+
+# My company projected annual profit calculator
+# 02/18/20
+# CTI-110 P2T1 - Sales Prediction
+# Jamey Heyward
+#
+
+total_sales = float(input(" Enter the projected sales: "))
+
+# Calculate the profit as 23 percent of the total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print ("The profit is $", format(profit, ",.2f"))
